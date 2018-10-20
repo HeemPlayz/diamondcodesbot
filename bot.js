bd254a3052df6927edd6c663927969f2c682140f
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-10-20 13:23:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`#help | DiamondCodes`,"http://twitch.tv/S-F")
+client.user.setGame(`#help-js | DiamondCodes`,"http://twitch.tv/S-F")
 client.user.setStatus("online")
  
 });
