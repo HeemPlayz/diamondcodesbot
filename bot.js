57abd0b14e7a068e4fd238fbaad862caf01b5b8d
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-10-20 11:06:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1723,5 +1723,148 @@ client.on ("guildMemberRemove", member => {
    
 }) 
 
+client.on('message', async rokz => {
+
+    if(rokz.content.startsWith(prefix + "تقديم")) {
+
+      let lang = '';
+
+      let time = '';
+
+      let expe = '';
+
+      let fillter = m => m.author.id === rokz.author.id
+
+      await rokz.channel.send("ما لغتك ؟").then(e => {
+
+     rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+
+     .then(co => {
+
+       lang = co.first().content;
+
+        co.first().delete();
+
+
+       e.edit(`كم هي المدة ؟
+[${lang}]`)
+
+       rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+
+       .then(col => {
+
+         time = col.first().content;
+
+          col.first().delete();
+
+
+            e.edit(`ما هي خبرتك ؟
+[${time}]
+[${lang}]`)
+
+            rokz.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+
+            .then(coll => {
+
+              expe = coll.first().content;
+
+               coll.first().delete();
+
+
+               e.edit(`جاري تقديمك...
+[${expe}]
+[${time}]
+[${lang}]`)
+
+              let rokzz = rokz.guild.channels.find("name","submissions")
+
+              setTimeout(() => {
+
+                e.edit("تم التقديم")
+
+              }, 3000)
+
+              rokzz.send(`
+» اللغة : **${lang}**
+» المدة : **${time}**
+» الخبرة : **${expe}**
+تم التقديم بواسطة: ${rokz.author}
+`).then(rokzzz => {
+
+                  rokzzz.react(":CheckMark:")
+
+                  rokzzz.react(":WrongMark:")
+
+                })
+
+            })
+
+       })
+
+     })
+
+   })
+
+    }
+
+  })
+
+ 
+
+    
+
+    
+client.on('message',async message => {
+
+let mention = message.mentions.members.first();
+
+let Room = client.channels.get('503116166936395787');
+
+if(message.content.startsWith(prefix + "رفض")) {
+
+if(message.guild.id !== '503116166936395787') return;
+
+ if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
+
+
+if(!mention) return message.reply("منشن شخص");
+
+
+
+Room.send(`
+**» العضو :** ${mention}
+[ :x: ] :: لقد تم رفض العضو`);
+
+}
+
+});
+  
+
+    
+client.on('message',async message => {
+
+let mention = message.mentions.members.first();
+
+let Room = client.channels.get('503116166936395787');
+
+if(message.content.startsWith(prefix + "قبول")) {
+
+if(message.guild.id !== '503116166936395787') return;
+
+ if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("**للأسف ليس لديك صلاحية**").then(msg => msg.delete(5000));
+
+
+if(!mention) return message.reply("منشن شخص");
+
+
+
+Room.send(`
+**» العضو :** ${mention}
+[ :white_check_mark: ] :: لقد تم قبول العضو واعطائه رتبة سبورت`);
+
+}
+
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
