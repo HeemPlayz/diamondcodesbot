37fc3c8e404d5f40c6b74912805de34181c1d511
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-10-26 01:03:01 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,6 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`#help-js | Alpha-Codes`,"http://twitch.tv/S-F")
-client.user.setStatus("online!")
   
   
      client.on('message', message => {
