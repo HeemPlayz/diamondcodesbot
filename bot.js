88949a292819c25f50181c956a0fe6e37d11e3a2
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-10-25 23:40:36 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const moment = require('moment');
 const client = new Discord.Client();
 const prefix = "#";
-const devs = ['383606619671494659' , '252813587188416512' , '383606619671494659' , ''];
+const devs = ['492552276326481930' , '488389313365540864' , '383606619671494659' , ''];
 const adminprefix = "#";
 
 client.on('ready', () => {
